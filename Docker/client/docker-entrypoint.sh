ea8957403172a5cb4eed01ea8d66958506498214
K. de Jong <kjong+dev@neobits.nl>
2019-06-18 11:24:01 +0200

    Client-server model works

--- Docker/client/docker-entrypoint.sh
+++ Docker/client/docker-entrypoint.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
 echo "Hostname: $HOSTNAME"
+echo "Gateway: $gateway"
 
 if nfd-start > /var/log/nfd.log; then
   echo "NFD started"
   sleep 2s
 else
   echo "NFD failed to start"
+  exit
+fi
+
+if nfdc face create udp4://"$gateway":6363; then
+  echo "Default NDN gateway set"
+  sleep 2s
+else
+  echo "Failed to set default NDN gateway"
+  exit
+fi
+
+if nfdc route add /example udp://"$gateway":6363; then
+  echo "Default NDN gateway set"
+  sleep 2s
+else
+  echo "Failed to set default NDN gateway"
   exit
 fi
 
